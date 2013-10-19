92302a3121037b74e9427eab7a164ec87635a8c8
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-10-19 12:14:08 -0400

    fixed has many

--- app/models/hackday_organization.rb
+++ app/models/hackday_organization.rb
@@ -1,5 +1,5 @@
 class HackdayOrganization < ActiveRecord::Base
-  has_many :hackdays, -> { order: "start_date desc" }
+  has_many :hackdays, -> { order("start_date desc") }
   has_many :users_hackday_organizations
   has_many :users, through: :users_hackday_organizations
   has_many :projects, through: :hackdays
