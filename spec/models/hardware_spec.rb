fa603708b2e35223ed0fe79beb883b994997a88f
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-10-19 11:20:16 -0400

    fixed form and adde some seed data

---
+++ spec/models/hardware_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Hardware do
+  describe '::for_hackday_organization' do
+    before do
+      @hackday_organization = FactoryGirl.create :hackday_organization
+    end
+    context 'none for org'
+  end
+end
