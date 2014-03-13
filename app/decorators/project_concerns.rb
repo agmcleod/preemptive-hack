0033761f3ec39a26fc6edb37893cd7d3730c8dfa
Aaron McLeod <aaron.mcleod@mosaic.com>
2014-03-13 15:42:08 -0400

    renamed to decorators

--- app/decorators/project_concerns.rb
+++
@@ -1,9 +0,0 @@
-module ProjectConcerns
-  def has_hardware_id?(id)
-    hardwares.collect(&:id).include? id
-  end
-
-  def hackday_organization
-    hackday && hackday.hackday_organization
-  end
-end
