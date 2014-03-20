6b9d6fbf4b2dfd570ab38c582c9916388b8220b3
Aaron McLeod <aaron.mcleod@mosaic.com>
2014-03-20 13:36:37 -0400

    edit user spec.

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'Feature: edit user' do
+  scenario 'change username' do
+    login_for_feature
+    click_link 'Edit My Info'
+    fill_in 'user_username', with: "anewname"
+    click_button 'Save'
+    expect(User.last.username).to eq('anewname')
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe User do
+  it 'user name should strip spaces' do
+    user = User.new username: "A user name"
+    user.save
+    expect(user.username).to eq('ausername')
+  end
+end
