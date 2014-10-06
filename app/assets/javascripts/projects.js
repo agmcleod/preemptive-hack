733db41bd42d390f4543b37c243ffdf40bacea72
Aaron McLeod <sircoolguy99@gmail.com>
2014-10-05 20:55:02 -0400

    fixed project tabs

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,8 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function () {
-  $('.nav-tabs a').on('click', function (e) {
-    e.preventDefault();
-    $(this).tab($(this).data('toggle'));
-  });
+  $('.nav-tabs').tab();
 });
